d50208b99c3e5606dac619aa0db151f4cadc6a6e
Walter Anderson <walter@kimbia.com>
2015-09-28 17:00:41 -0500

    adding gitconfig

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 cd ~
 
+rm -f .gitconfig
 rm -f .bashrc
 rm -f .bash_profile
 rm -rf .vim
 rm -rf .vimrc
 
+ln -s ~/.dotfiles/gitconfig .gitconfig
 ln -s ~/.dotfiles/bashrc .bashrc
 ln -s ~/.dotfiles/bash_profile .bash_profile
 ln -s ~/.dotfiles/vim .vim
