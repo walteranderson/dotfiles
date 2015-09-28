2fc769a989e190464ad304fb82f607cba916d5c0
Walter Anderson <walter@kimbia.com>
2015-09-28 17:03:11 -0500

    adding git completion

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 cd ~
 
 rm -f .gitconfig
+rm -f .git-completion.bash
 rm -f .bashrc
 rm -f .bash_profile
 rm -rf .vim
 rm -rf .vimrc
 
 ln -s ~/.dotfiles/gitconfig .gitconfig
+ln -s ~/.dotfiles/git-completion.bash .git-completion.bash
 ln -s ~/.dotfiles/bashrc .bashrc
 ln -s ~/.dotfiles/bash_profile .bash_profile
 ln -s ~/.dotfiles/vim .vim
