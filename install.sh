3c738a42b79f23b91d76730a3e0ba64d5c987a68
Walter Anderson <walter@kimbia.com>
2015-09-28 22:03:00 -0500

    adding oh-my-zsh

--- install.sh
+++ install.sh
@@ -6,12 +6,13 @@ rm -f .bashrc
 rm -f .bash_profile
 rm -rf .vim
 rm -rf .vimrc
+rm -rf .zshrc
 
+ln -s ~/.dotfiles/zshrc .zshrc
 ln -s ~/.dotfiles/gitconfig .gitconfig
 ln -s ~/.dotfiles/git-completion.bash .git-completion.bash
 ln -s ~/.dotfiles/bashrc .bashrc
 ln -s ~/.dotfiles/bash_profile .bash_profile
 ln -s ~/.dotfiles/vim .vim
 ln -s ~/.dotfiles/vim/vimrc .vimrc
-
 
