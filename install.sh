d8b246db48ec655d2784d022d1b723aaeea4002a
Walter Anderson <walter@kimbia.com>
2015-11-17 22:23:53 -0600

    some new bundles, adding tmux

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ rm -rf .vim
 rm -rf .vimrc
 rm -rf .gvimrc
 rm -rf .zshrc
+rm -rf .tmux.conf
 
 ln -s ~/.dotfiles/zshrc .zshrc
 ln -s ~/.dotfiles/gitconfig .gitconfig
@@ -17,5 +18,6 @@ ln -s ~/.dotfiles/bash_profile .bash_profile
 ln -s ~/.dotfiles/vim .vim
 ln -s ~/.dotfiles/vim/vimrc .vimrc
 ln -s ~/.dotfiles/vim/gvimrc .gvimrc
+ln -s ~/.dotfiles/tmux.conf  .tmux.conf
 
 cp ~/.dotfiles/custom.zsh-theme ~/.dotfiles/zsh/themes/custom.zsh-theme
