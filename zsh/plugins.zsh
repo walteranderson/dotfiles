64c5dfa67ceffabf0720648647dc59d63e1255ed
Walter Anderson <walter@kimbia.com>
2018-04-06 12:11:55 -0500

    I installed nvm manually instead of through homebrew

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -5,11 +5,11 @@ source ~/.zsh/plugins/git-flow.zsh
 source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # load nvm
-if type brew > /dev/null; then
-  source /usr/local/opt/nvm/nvm.sh
-else
+# if type brew > /dev/null; then
+#   source /usr/local/opt/nvm/nvm.sh
+# else
   source $HOME/.nvm/nvm.sh
-fi
+# fi
 
 # Load RVM into a shell session *as a function*
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
