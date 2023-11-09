59a7be3ec2eab0335c561d23571c5f7f32e339f9
Walter Anderson <walter.anderson@harborhealthteam.com>
2023-11-09 09:53:25 -0600

    using telescope live grep

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -24,6 +24,4 @@ setup({
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>j', builtin.buffers, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep >") })
-end)
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
