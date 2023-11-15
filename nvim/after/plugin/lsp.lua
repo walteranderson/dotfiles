e141cf2f518c4620c2c92bf868d8e3a265608dd4
Walter Anderson <walter.anderson@harborhealthteam.com>
2023-11-15 09:56:33 -0600

    swapping copilot plugins

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,54 +3,59 @@ local lsp = require('lsp-zero').preset({})
 local cmp = require('cmp')
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+  lsp.default_keymaps({ buffer = bufnr })
   if nvim_lsp.util.root_pattern("deno.json", "import_map.json")(vim.fn.getcwd()) then
-        if client.name == "tsserver" then
-            client.stop()
-            return
-        end
+    if client.name == "tsserver" then
+      client.stop()
+      return
     end
+  end
 end)
 
 lsp.format_on_save({
-    format_opts = {
-        timeout_ms = 10000,
-    },
-    servers = {
-        ['null-ls'] = { 'javascript', 'typescript', 'lua', 'svelte' }
-    }
+  format_opts = {
+    timeout_ms = 10000,
+  },
+  servers = {
+    ['null-ls'] = { 'javascript', 'typescript', 'lua', 'svelte' }
+  }
 })
 
 lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-	'svelte'
+  'tsserver',
+  'eslint',
+  'svelte'
 })
 
 lsp.configure('denols', {
-    root_dir = nvim_lsp.util.root_pattern("deno.json", "import_map.json"),
+  root_dir = nvim_lsp.util.root_pattern("deno.json", "import_map.json"),
 })
 
 cmp.setup({
-    enabled = function()
-        -- disable completion in comments
-        local context = require 'cmp.config.context'
+  preselect = cmp.PreselectMode.None,
+  sources = {
+    { name = "copilot" },
+    { name = "nvim_lsp" },
+  },
+  enabled = function()
+    -- disable completion in comments
+    local context = require 'cmp.config.context'
 
-        -- keep command mode completion enabled when cursor is in a comment
-        if vim.api.nvim_get_mode().mode == 'c' then
-            return true
-        else
-            return not context.in_treesitter_capture('comment')
-                and not context.in_syntax_group('Comment')
-        end
-    end,
-	mapping = {
-		-- `Enter` key to confirm completion
-		['<CR>'] = cmp.mapping.confirm({select = false}),
+    -- keep command mode completion enabled when cursor is in a comment
+    if vim.api.nvim_get_mode().mode == 'c' then
+      return true
+    else
+      return not context.in_treesitter_capture('comment')
+          and not context.in_syntax_group('Comment')
+    end
+  end,
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
 
-		-- Ctrl+Space to trigger completion menu
-		['<C-Space>'] = cmp.mapping.complete()
-	}
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete()
+  }
 })
 
 -- (Optional) Configure lua language server for neovim
@@ -62,11 +67,11 @@ local null_ls = require('null-ls')
 local prettier = require('prettier')
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.prettier.with({
-            extra_filetypes = { 'svelte' }
-        })
-    }
+  sources = {
+    null_ls.builtins.formatting.prettier.with({
+      extra_filetypes = { 'svelte' }
+    })
+  }
 })
 
 prettier.setup()
--- nvim/lua/wanderson/packer.lua
+++ nvim/lua/wanderson/packer.lua
@@ -10,23 +10,24 @@ return require('packer').startup(function(use)
     'nvim-treesitter/nvim-treesitter',
     { run = ':TSUpdate' }
   }
+
   use {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v2.x',
-      requires = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
-        {                                      -- Optional
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' }, -- Required
+      {                            -- Optional
         'williamboman/mason.nvim',
         run = function()
           pcall(vim.cmd, 'MasonUpdate')
         end,
       },
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
+      { 'hrsh7th/nvim-cmp' },                  -- Required
+      { 'hrsh7th/cmp-nvim-lsp' },              -- Required
+      { 'L3MON4D3/LuaSnip' },                  -- Required
     }
   }
   use('jose-elias-alvarez/null-ls.nvim')
@@ -38,7 +39,7 @@ return require('packer').startup(function(use)
   -- fuzzy finder
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
   -- harpoon
@@ -71,7 +72,25 @@ return require('packer').startup(function(use)
   }
 
   -- Github Copilot
-  use 'github/copilot.vim'
+  -- use 'github/copilot.vim'
+  use {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  }
+  use {
+    "zbirenbaum/copilot-cmp",
+    after = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end
+  }
 
   -- matching html tags
   use 'leafOfTree/vim-matchtag'
