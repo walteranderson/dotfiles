c87f2cfcfc6642af5b9e3426a913ca07f872b44f
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-02-11 11:34:59 -0600

    configuring lualine

--- kickstart-nvim/lua/custom/plugins/lualine.lua
+++ kickstart-nvim/lua/custom/plugins/lualine.lua
@@ -1,13 +1,11 @@
 return {
-  -- Set lualine as statusline
   'nvim-lualine/lualine.nvim',
-  -- See `:help lualine.txt`
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
-    -- options = {
-    --   icons_enabled = false,
-    --   theme = 'auto',
-    --   component_separators = '|',
-    --   section_separators = '',
-    -- },
+    options = {
+      icons_enabled = true,
+      component_separators = '',
+      section_separators = '',
+    },
   },
 }
