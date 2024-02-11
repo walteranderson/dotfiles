a16a5ef501082a9cbb683f7b352b28c2f236b409
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-02-11 11:25:08 -0600

    load colorscheme first

--- kickstart-nvim/lua/custom/plugins/colorscheme.lua
+++ kickstart-nvim/lua/custom/plugins/colorscheme.lua
@@ -1,5 +1,7 @@
 return {
   'EdenEast/nightfox.nvim',
+  lazy = false,      -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000,   -- make sure to load this before all the other start plugins
   config = function()
     vim.cmd.colorscheme('nordfox')
   end
