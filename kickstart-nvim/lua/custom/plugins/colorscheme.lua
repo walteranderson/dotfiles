e30b47fd8a752f4fba0226310d419952509013fa
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-04-25 13:39:59 -0500

    toggle colorscheme light/dark

--- kickstart-nvim/lua/custom/plugins/colorscheme.lua
+++ kickstart-nvim/lua/custom/plugins/colorscheme.lua
@@ -1,8 +1,25 @@
+local COLOR = 'nordfox'
+
 return {
   'EdenEast/nightfox.nvim',
-  lazy = false,      -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000,   -- make sure to load this before all the other start plugins
+  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  keys = {
+    {
+      "<leader>tc",
+      function()
+        if COLOR == 'nordfox' then
+          COLOR = 'dawnfox'
+        else
+          COLOR = 'nordfox'
+        end
+
+        vim.cmd.colorscheme(COLOR)
+      end,
+      desc = "[T]oggle [C]olorscheme",
+    },
+  },
   config = function()
-    vim.cmd.colorscheme('nordfox')
+    vim.cmd.colorscheme(COLOR)
   end
 }
