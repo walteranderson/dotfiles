fce9837f36c564f423728d69018ec8a9ee66b336
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-08-04 17:35:47 -0500

    removing tmuxinator stuff

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -6,9 +6,3 @@ zstyle ':completion:*' list-colors '' # use ls colors
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
 compdef -d mpv
-
-# tmuxinator completion
-# only add if tmuxinator is present
-if type tmuxinator > /dev/null; then
-    source ~/.zsh/completion/tmuxinator.zsh
-fi
