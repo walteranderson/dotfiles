9fff436655345228fe63c31e55bd7b445b363f36
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-08-23 14:11:56 -0500

    fixing the dumb neovim default keybinds in sql files

--- .config/nvim/lua/custom/plugins/dadbod.lua
+++ .config/nvim/lua/custom/plugins/dadbod.lua
@@ -1,8 +1,11 @@
+vim.g.db_ui_use_nerd_fonts = 1
+vim.g.db_ui_use_postgres_views = 0
+
+-- this is something NEOVIM adds... Why the fuck would you add keybinds to ctrl-c?????
+vim.g.omni_sql_no_default_maps = 1
+
 return {
   'tpope/vim-dadbod',
   'kristijanhusak/vim-dadbod-completion',
   'kristijanhusak/vim-dadbod-ui',
-  keys = {
-    { '<leader>db', '<cmd>DBUI<cr>' },
-  },
 }
