af1a3b428832fa65968d1d3ea043ebf7df03337d
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-10-09 08:59:11 +0100

    closing oil with esc instead of ctrl-c

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -9,6 +9,8 @@ return {
         ['<C-k>'] = false,
         ['<C-j>'] = false,
         ['<M-h>'] = 'actions.select_split',
+        ['<C-c>'] = false,
+        ['<Esc>'] = 'actions.close',
       },
       view_options = {
         show_hidden = true,
