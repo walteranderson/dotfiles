2dcbacbd013284460cafcef174a66caf22f8c61e
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-11-07 11:36:11 -0600

    adding more split management keybinds

--- .config/nvim/lua/custom/plugins/smart-splits.lua
+++ .config/nvim/lua/custom/plugins/smart-splits.lua
@@ -7,9 +7,22 @@ return {
 
     -- resizing splits
     set('n', '<A-h>', smart_splits.resize_left)
+    set('n', '<S-A-h>', function()
+      smart_splits.resize_left(100)
+    end)
     set('n', '<A-j>', smart_splits.resize_down)
+    set('n', '<S-A-j>', function()
+      smart_splits.resize_down(100)
+    end)
     set('n', '<A-k>', smart_splits.resize_up)
+    set('n', '<S-A-k>', function()
+      smart_splits.resize_up(100)
+    end)
     set('n', '<A-l>', smart_splits.resize_right)
+    set('n', '<S-A-l>', function()
+      smart_splits.resize_right(100)
+    end)
+    set('n', '<A-=>', '<C-w>=<CR>')
 
     -- moving between splits
     set('n', '<C-h>', smart_splits.move_cursor_left)
