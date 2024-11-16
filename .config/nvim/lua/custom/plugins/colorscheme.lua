d5690c03e4f79c8908d5505ecc7bf5b88bff1900
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-11-16 15:10:06 -0600

    colorscheme update

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,24 +1,53 @@
 local COLOR = 'nordfox'
 
 return {
-  'EdenEast/nightfox.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme(COLOR)
-  end,
-  keys = {
-    {
-      '<leader>tc',
-      function()
-        if COLOR == 'nordfox' then
-          COLOR = 'dawnfox'
-        else
-          COLOR = 'nordfox'
-        end
+  {
+    'EdenEast/nightfox.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- 3 Shades: base, bright, dim
+      -- Available colors:
+      --   black, red, green, yellow, blue,
+      --   magenta, cyan, white, orange, pink
 
-        vim.cmd.colorscheme(COLOR)
-      end,
+      require('nightfox').setup {
+        options = {
+          transparent = true,
+        },
+        palettes = {
+          nordfox = {
+            white = '#bababa',
+            -- cyan = { base = '#ff0000', bright = '#00ff00', dim = '#0000ff' },
+          },
+        },
+        specs = {
+          nordfox = {
+            syntax = {
+              builtin1 = 'white.dim',
+              -- type = 'yellow.dim',
+            },
+          },
+        },
+      }
+      vim.cmd.colorscheme(COLOR)
+
+      -- local spec = require('nightfox.spec').load 'nordfox'
+      -- print(vim.inspect(spec))
+    end,
+    keys = {
+      {
+        '<leader>tc',
+        function()
+          if COLOR == 'nordfox' then
+            COLOR = 'dawnfox'
+          else
+            COLOR = 'nordfox'
+          end
+
+          vim.cmd.colorscheme(COLOR)
+        end,
+      },
     },
   },
 }
