8cf00d319af6bb5ea4fb0d34e22bc508aa5eccf8
Walter Anderson <walter.anderson@harborhealthteam.com>
2024-11-20 16:09:51 -0600

    trying out a new colorscheme

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,53 +1,72 @@
-local COLOR = 'nordfox'
+-- local COLOR = 'nordfox'
 
 return {
   {
-    'EdenEast/nightfox.nvim',
-    lazy = false,
-    priority = 1000,
+    'rebelot/kanagawa.nvim',
     config = function()
-      -- 3 Shades: base, bright, dim
-      -- Available colors:
-      --   black, red, green, yellow, blue,
-      --   magenta, cyan, white, orange, pink
-
-      require('nightfox').setup {
-        options = {
-          transparent = true,
-        },
-        palettes = {
-          nordfox = {
-            white = '#bababa',
-            -- cyan = { base = '#ff0000', bright = '#00ff00', dim = '#0000ff' },
-          },
-        },
-        specs = {
-          nordfox = {
-            syntax = {
-              builtin1 = 'white.dim',
-              -- type = 'yellow.dim',
+      require('kanagawa').setup {
+        transparent = true,
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = 'none',
+              },
             },
           },
         },
       }
-      vim.cmd.colorscheme(COLOR)
-
-      -- local spec = require('nightfox.spec').load 'nordfox'
-      -- print(vim.inspect(spec))
+      vim.cmd.colorscheme 'kanagawa-dragon'
     end,
-    keys = {
-      {
-        '<leader>tc',
-        function()
-          if COLOR == 'nordfox' then
-            COLOR = 'dawnfox'
-          else
-            COLOR = 'nordfox'
-          end
-
-          vim.cmd.colorscheme(COLOR)
-        end,
-      },
-    },
   },
+
+  -- {
+  --   'EdenEast/nightfox.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     -- 3 Shades: base, bright, dim
+  --     -- Available colors:
+  --     --   black, red, green, yellow, blue,
+  --     --   magenta, cyan, white, orange, pink
+  --
+  --     require('nightfox').setup {
+  --       options = {
+  --         transparent = true,
+  --       },
+  --       palettes = {
+  --         nordfox = {
+  --           white = '#bababa',
+  --           -- cyan = { base = '#ff0000', bright = '#00ff00', dim = '#0000ff' },
+  --         },
+  --       },
+  --       specs = {
+  --         nordfox = {
+  --           syntax = {
+  --             builtin1 = 'white.dim',
+  --             -- type = 'yellow.dim',
+  --           },
+  --         },
+  --       },
+  --     }
+  --     vim.cmd.colorscheme(COLOR)
+  --
+  --     -- local spec = require('nightfox.spec').load 'nordfox'
+  --     -- print(vim.inspect(spec))
+  --   end,
+  --   keys = {
+  --     {
+  --       '<leader>tc',
+  --       function()
+  --         if COLOR == 'nordfox' then
+  --           COLOR = 'dawnfox'
+  --         else
+  --           COLOR = 'nordfox'
+  --         end
+  --
+  --         vim.cmd.colorscheme(COLOR)
+  --       end,
+  --     },
+  --   },
+  -- },
 }
