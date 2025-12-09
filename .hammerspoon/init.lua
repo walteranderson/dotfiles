32e26ccd75906a5e69a929f5c2f5d3b443d7bb35
Walter Anderson <walteranderson19@gmail.com>
2025-12-09 12:10:39 -0600

    cleanup

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,16 +2,12 @@ hs.hotkey.bind({ "ctrl" }, "`", function()
 	local name = "Alacritty"
 	local app = hs.application.get(name)
 	if app then
-		-- Check if the application is currently frontmost (active)
 		if app:isFrontmost() then
-			-- If frontmost, hide it
 			app:hide()
 		else
-			-- If not frontmost, activate it (which unhides it if it was hidden)
 			app:activate()
 		end
 	else
-		-- Optional: Launch the application if it's not running
 		hs.application.launchOrFocus(name)
 	end
 end)
